8501ff2ee74c2d417796769c5ce7d7b2d66ee0e0
Shabbar Adamjee <adamjeeshabbar@gmail.com>
2024-08-20 20:42:54 +0530

    Aug 20th

--- FSD/Assgn3/calc/script.js
+++ FSD/Assgn3/calc/script.js
@@ -1,50 +1,120 @@
-const form = document.getElementsByTagName('form')
-const sumBtn = document.getElementById("sum");
-const subBtn = document.getElementById("sub");
-const mulBtn = document.getElementById("mul");
-const divBtn = document.getElementById("div");
+const one = document.getElementById("one")
+const two = document.getElementById("two")
+const three = document.getElementById("three")
+const four = document.getElementById("four")
+const five = document.getElementById("five")
+const six = document.getElementById("six")
+const seven = document.getElementById("seven")
+const eight = document.getElementById("eight")
+const nine = document.getElementById("nine")
+const zero = document.getElementById("zero")
+
+const plus = document.getElementById("plus")
+const minus = document.getElementById("minus")
+const multiply = document.getElementById("multiply")
+const divide = document.getElementById("divide")
+const equal = document.getElementById("equal")
+const clr = document.getElementById("clr")
+
+const numButtons = [one, two, three, four, five, six, seven, eight, nine, zero];
+const opButtons = [plus, minus, multiply, divide, equal, clr];
+
 let ans = document.getElementById("ans")
+ans.innerText = "";
 
-form[0].reset()
-let count = 0;
+let num1 = null;
+let num2 = null;
+let op = null;
+let toDo = null;
 
-function calcFn(type) {
-  let val1 = document.getElementById("val1").value;
-  let val2 = document.getElementById("val2").value;
+for (let i = 0; i < numButtons.length; i++) {
+  const element = numButtons[i];
+  element.onclick = function() { enterNumber(element.value) };
+}
 
-  if (val1 === "" || val2 === "") {
-    alert("Fill it all");
-    count = count + 1;
-    return;
-  }
+for (let i = 0; i < opButtons.length; i++) {
+  const element = opButtons[i];
+  element.onclick = function() { performOp(element.value) };
+}
 
-  val1 = Number(val1);
-  val2 = Number(val2);
-
+function enterNumber(type) {
   switch (type) {
-    case "+": {
-      ans.innerText = val1 + val2;
+    case "1":
+      ans.innerText += "1";
       break;
-    }
-    case "-": {
-      ans.innerText = val1 - val2;
+    case "2":
+      ans.innerText += "2";
       break;
-    }
-    case "*": {
-      ans.innerText = val1 * val2;
+    case "3":
+      ans.innerText += "3";
       break;
-    }
-    case "/": {
-      ans.innerText = val1 / val2;
+    case "4":
+      ans.innerText += "4";
       break;
-    }
-    default:
+    case "5":
+      ans.innerText += "5";
+      break;
+    case "6":
+      ans.innerText += "6";
+      break;
+    case "7":
+      ans.innerText += "7";
+      break;
+    case "8":
+      ans.innerText += "8";
+      break;
+    case "9":
+      ans.innerText += "9";
+      break;
+    case "0":
+      ans.innerText += "0";
       break;
   }
 }
 
-sumBtn.onclick = function() { calcFn("+"); };
-subBtn.onclick = function() { calcFn("-"); };
-mulBtn.onclick = function() { calcFn("*"); };
-divBtn.onclick = function() { calcFn("/"); };
-
+function performOp(op) {
+  switch (op) {
+    case "+":
+      toDo = "+";
+      num1 = Number(ans.innerText);
+      ans.innerText = "";
+      break;
+    case "-":
+      toDo = "-";
+      num1 = Number(ans.innerText);
+      ans.innerText = "";
+      break;
+    case "*":
+      toDo = "*";
+      num1 = Number(ans.innerText);
+      ans.innerText = "";
+      break;
+    case "/":
+      toDo = "/";
+      num1 = Number(ans.innerText);
+      ans.innerText = "";
+      break;
+    case "=":
+      num2 = Number(ans.innerText);
+      switch (toDo) {
+        case "+":
+          ans.innerText = num1 + num2;
+          break;
+        case "-":
+          ans.innerText = num1 - num2;
+          break;
+        case "*":
+          ans.innerText = num1 * num2;
+          break;
+        case "/":
+          ans.innerText = num1 / num2;
+          break;
+      }
+      break;
+    case "clr":
+      ans.innerText = "";
+      num1 = null;
+      num2 = null;
+      break;
+  }
+}
